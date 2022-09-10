2066a316b66d768601e3ef402c72f47d4d009962
jonas <pedrojonas.pj77@gmail.com>
2022-09-10 15:51:46 -0300

    Post sign-in implementado

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import joi from 'joi';
 import bcrypt from 'bcrypt';
+import {v4 as uuid} from 'uuid'
 
 dotenv.config();
 const server = express();
@@ -18,6 +19,11 @@ mongoClient.connect().then(()=>{
 
 const signupSchema = joi.object({
     name: joi.string().required().min(1),
+    email: joi.string().required().email().min(1),
+    password: joi.string().required().min(1),
+});
+
+const signinSchema = joi.object({
     email: joi.string().required().email().min(1),
     password: joi.string().required().min(1),
 });
@@ -40,11 +46,37 @@ server.post('/sign-up', async (req, res)=>{
             };
         };
         const singup = await db.collection('users').insertOne({...user, password: passwordHash});
-        res.status(201).send(passwordHash);
+        res.status(201).send("ok");
     } catch {
         res.sendStatus(422);
     };
+});
 
+server.post('/sign-in', async(req,res)=>{
+    const {email, password} = req.body;
+    const validation = signinSchema.validate({email, password});
+    if (validation.error){
+        res.status(422).send("error");
+        return;
+    };
+
+    try{
+        const user = await db.collection('users').findOne({email});
+        const validatePassword = bcrypt.compareSync(password, user.password);
+
+        if (user && validatePassword){
+            const token = uuid();
+            await db.collection("sessions").insertOne({
+                userId: user._id,
+                token
+            })
+            return res.send(token);
+        } else {
+            return res.sendStatus(422).send("password invalid");
+        };
+    } catch {
+        res.sendStatus(422);
+    };
 });
 
 server.listen(5000);
