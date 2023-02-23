428b57ed353d5d582289b5f430c30d8172302111
jonas <pedrojonas.pj77@gmail.com>
2023-02-23 15:12:13 -0300

    config: deploy vercel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express, { json } from 'express';
 import cors from 'cors';
 
+const port = process.env.PORT || 5000;
 import registersRouter from './routes/registers.Routes.js';
 import authRouter from './routes/auth.Routes.js';
 
@@ -11,4 +12,4 @@ server.use(json());
 server.use(authRouter);
 server.use(registersRouter);
 
-server.listen(5000);
+server.listen(port);
